7f3e7a80a560d244ab5d01dcf2a8fae7126d6d67
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2018-07-04 18:31:52 +0200

    updated animations

--- arduinoLightDimmer.ino
+++ arduinoLightDimmer.ino
@@ -4,15 +4,17 @@
 
 #define INTERRUPT_PIN 2
 
-const byte us = 75;
-unsigned int dimtime;
-byte dimming = 149;  // 140 |  Dimming level (0-128)  0 = ON, 128 = OFF | najniższy poziom, do którego się ściemnia | im więcej tym ciemniej
+const byte scaleDivider = 62;
+unsigned int dimtime; // czas w mikrosekundach, wynik mnożenia scaleDivider*dimming
+byte dimming = 150;  // Dimming level (8-128)  8 = ON, 150 = OFF | najniższy poziom, do którego się ściemnia | im więcej tym ciemniej
 byte dimmingMax = dimming;
-byte dimmingMin = 50; //50 has the brightest light| najwyższy poziom, do którego się rozjaśnia | im mniej tym jaśniej
-unsigned int animationTime = 100;
+byte dimmingMin = 100; //8 has the brightest light| najwyższy poziom, do którego się rozjaśnia | im mniej tym jaśniej
+unsigned int animationTime = 20;
 byte bulbPinNumber;
-const byte animationType = 1;
-boolean triggerFlag = true;
+boolean bulbDirection = true; // true - liczy w górę | false - liczy w dół
+const byte animationProgram = 5;
+boolean triggerFlag = true; 
+volatile boolean triggerFlagInterrupt = true; 
 
 /*
  * Programy:
@@ -20,11 +22,14 @@ boolean triggerFlag = true;
  * 2) Od lewej do prawej pojedyncza żarówka się rozjasnia i ściemnia
  * 3) Od prawej do lewej pojedyncza żarówka się rozjasnia i ściemnia
  * 4) Wszystkie powoli się rozjaśniają do pewnego poziomu i utrzymują go
+ * 5) Od lewej do prawej jedna za drugą się rozjaśnia 
+ * 
+ * x) Sterowanie jasnością każdej żarówki pojedynczo
  */
 
 
 void setup() {
-  
+
   pinMode(BULB_PIN_1, OUTPUT);
   pinMode(BULB_PIN_2, OUTPUT);
   pinMode(BULB_PIN_3, OUTPUT);
@@ -32,66 +37,68 @@ void setup() {
   digitalWrite(BULB_PIN_1, LOW);
   digitalWrite(BULB_PIN_2, LOW);
   digitalWrite(BULB_PIN_3, LOW);
-  
+
   attachInterrupt(
     digitalPinToInterrupt(INTERRUPT_PIN), 
     zeroCrosssInterrupt,
     RISING
   );
 
-  if (animationType == 2) {
+  if (animationProgram == 2 || animationProgram == 5) {
     bulbPinNumber = 4;
-  } else if (animationType == 3) {
+  } else if (animationProgram == 3 || animationProgram == 6) {
     bulbPinNumber = 6;
   }  
 }
 
-//the interrupt function must take no parameters and return nothing
+
+
+
 void zeroCrosssInterrupt() {
-  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
-  // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle) 
-  // For 60Hz => 8.33ms (10.000/120)
-  // 10ms=10000us
-  // (10000us - 10us) / 128 = 75 (Approx) For 60Hz => 65
-  /*
-   * 9990 / 50 = 199 us
-   * 9990 / 40 = 249 us
-   * 
-   */
+
   // 500 najjasniej
   // 8500 najciemniej, ledwo się żarzy
-  // 
-  
-  //dimtime = (us*dimming);    // For 60Hz => 65
-  
-  dimtime = 8000; 
-  delayMicroseconds(dimtime);    // Wait till firing the TRIAC  
+  // 8000 bardzo ciemno, komfortowo dla oczu, ładnie wygląda
 
-  if (animationType == 1 || animationType == 4 ) {
+if ( bulbPinNumber == INTERRUPT_PIN ) { // zabezpieczenie, gdyby numer pinu wszedł na pin przerwania
+  bulbPinNumber = BULB_PIN_1;
+}
+
+  dimtime = scaleDivider*dimming; 
+//  dimtime = 8500; 
+  delayMicroseconds(dimtime);
+
+  if (animationProgram == 1 || animationProgram == 4 ) {
     
-//    digitalWrite(BULB_PIN_1, HIGH);   // Fire the TRIAC
-//    digitalWrite(BULB_PIN_2, HIGH);
+    digitalWrite(BULB_PIN_1, HIGH);
+    digitalWrite(BULB_PIN_2, HIGH);
     digitalWrite(BULB_PIN_3, HIGH);
-    delayMicroseconds(10);         // triac On propogation delay 
-//    digitalWrite(BULB_PIN_1, LOW);    // No longer trigger the TRIAC (the next zero crossing will swith it off)
-//    digitalWrite(BULB_PIN_2, LOW);
+    delayMicroseconds(10); 
+    digitalWrite(BULB_PIN_1, LOW); 
+    digitalWrite(BULB_PIN_2, LOW);
     digitalWrite(BULB_PIN_3, LOW);
 
-//    delayMicroseconds(1000);
-//    digitalWrite(BULB_PIN_2, HIGH);
-//    delayMicroseconds(10);
-//    digitalWrite(BULB_PIN_2, LOW);
-//
-//    delayMicroseconds(1000);
-//    digitalWrite(BULB_PIN_3, HIGH);
-//    delayMicroseconds(10);
-//    digitalWrite(BULB_PIN_3, LOW);
-    
-  } else if (animationType == 2 || animationType == 3) {
+  } else if (animationProgram == 2 || animationProgram == 3 || animationProgram == 5) {
     
     digitalWrite(bulbPinNumber, HIGH);
     delayMicroseconds(10);
     digitalWrite(bulbPinNumber, LOW);
+  
+  } else if (animationProgram == 999) {
+
+    digitalWrite(BULB_PIN_3, HIGH);
+    delayMicroseconds(10);      
+    digitalWrite(BULB_PIN_3, LOW);
+
+    delayMicroseconds(5000);
+    digitalWrite(BULB_PIN_2, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(BULB_PIN_2, LOW);
+
+    delayMicroseconds(1000);
+    digitalWrite(BULB_PIN_1, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(BULB_PIN_1, LOW);
   
   }
 
@@ -101,9 +108,9 @@ void zeroCrosssInterrupt() {
 
 
 void loop()  {
-delay(100);
-//  if (animationType >= 1  && animationType <= 3) {
-  if (animationType == 9) {
+
+  if (animationProgram >= 1  && animationProgram <= 3 || animationProgram == 5) {
+
     for (int i=dimmingMax; i>=dimmingMin; i--) { // rozjasnianie
       dimming = i;
       delay(animationTime);
@@ -114,25 +121,36 @@ delay(100);
       delay(animationTime);
     } 
   
-    if (animationType == 2) {
+    if (animationProgram == 2) {
       bulbPinNumber++;
       if (bulbPinNumber > 6) {
         bulbPinNumber = 4;
       }
-    } else if (animationType == 3) {
+    } else if (animationProgram == 3) {
       bulbPinNumber--;
       if (bulbPinNumber < 4) {
         bulbPinNumber = 6;
       }    
+    } else if (animationProgram == 5) {
+      if (bulbPinNumber <= 4) {
+        bulbPinNumber++;
+        bulbDirection = true;
+      } else if (bulbPinNumber >= 6) {
+        bulbPinNumber--; 
+        bulbDirection = false;
+      } else {
+        if (bulbDirection) {
+          bulbPinNumber++;
+        } else {
+          bulbPinNumber--;
+        }
+      }
     }
     
-  } else if (animationType == 4) {
-//Dane:
-//unsigned int dimming = 149;
-//unsigned int animationTime = 100;
+  } else if (animationProgram == 4) {
 
     if ( triggerFlag ) {
-      for (int i=dimmingMax; i>=dimmingMax/2; i--) { // rozjasnianie
+      for (byte i=dimmingMax; i>=dimmingMin; i--) { // rozjasnianie
         dimming = i;
         delay(animationTime);
       }  
